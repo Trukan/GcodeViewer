76ca851981b60d8ec3f7833525ad6c7fa35e4f63
Petr Trukan <pitter@narod.ru>
2017-11-09 23:52:46 +0500

    добавлена тестовое заполнение таблицы, авто-нумерация строк, еще поправлены размеры областей, не хватало длины для отображения команд

--- GcodeViewer/MainForm.cpp
+++ GcodeViewer/MainForm.cpp
@@ -11,3 +11,39 @@ int main(array<String^>^ args)
 	GcodeViewer::MainForm Form;
 	Application::Run(%Form);
 }
+
+cli::array<String^>^ GcodeViewer::MainForm::getText(String^ filePath)
+{
+	cli::array<String^>^ tesText;
+	tesText = gcnew array<String^>(28) {
+		"T1M6",
+			"G17",
+			"G0Z10.000",
+			"G0X0.000Y0.000S18000M3",
+			"G0X7.000Y50.000Z10.000",
+			"G1Z - 3.000F1200.0",
+			"G1Y170.000F2400.0",
+			"G2X50.000Y213.000I43.000J0.000",
+			"G1X170.000",
+			"G2X213.000Y170.000I0.000J - 43.000",
+			"G1Y50.000",
+			"G2X170.000Y7.000I - 43.000J - 0.000",
+			"G1X50.000Y7.000",
+			"G2X7.000Y50.000I - 0.000J43.000",
+			"G1Z - 6.000F1200.0",
+			"G1Y170.000F2400.0",
+			"G2X50.000Y213.000I43.000J0.000",
+			"G1X170.000",
+			"G2X213.000Y170.000I0.000J - 43.000",
+			"G1Y50.000",
+			"G2X170.000Y7.000I - 43.000J - 0.000",
+			"G1X50.000Y7.000",
+			"G2X7.000Y50.000I - 0.000J43.000",
+			"G0Z10.000",
+			"G0X0.000Y0.000",
+			"G0Z10.000",
+			"G0X0.000Y0.000",
+			"M30"
+	};
+	return tesText;
+}
--- GcodeViewer/MainForm.h
+++ GcodeViewer/MainForm.h
@@ -8,6 +8,9 @@ namespace GcodeViewer {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace OpenTK;
+	using namespace OpenTK::Platform::Windows;
+	using namespace OpenTK::Graphics::OpenGL;
 
 	/// <summary>
 	/// Сводка для MainForm
@@ -22,7 +25,7 @@ namespace GcodeViewer {
 			//TODO: добавьте код конструктора
 			//
 		}
-
+		cli::array<String^>^ getText(String^);
 	protected:
 		/// <summary>
 		/// Освободить все используемые ресурсы.
@@ -59,8 +62,9 @@ namespace GcodeViewer {
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		 String^ opndfileName = "";
-
+		//имя открываемого файла
+		String^ opndfileName = "";
+		cli::array<String^>^ tText;
 #pragma region Windows Form Designer generated code
 		/// <summary>
 		/// Требуемый метод для поддержки конструктора — не изменяйте 
@@ -99,9 +103,9 @@ namespace GcodeViewer {
 			// 
 			this->glControl1->BackColor = System::Drawing::Color::Black;
 			this->glControl1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
-			this->glControl1->Location = System::Drawing::Point(212, 28);
+			this->glControl1->Location = System::Drawing::Point(360, 28);
 			this->glControl1->Name = L"glControl1";
-			this->glControl1->Size = System::Drawing::Size(793, 547);
+			this->glControl1->Size = System::Drawing::Size(643, 547);
 			this->glControl1->TabIndex = 0;
 			this->glControl1->VSync = true;
 			this->glControl1->AutoSizeChanged += gcnew System::EventHandler(this, &MainForm::MainForm_Resize);
@@ -164,18 +168,21 @@ namespace GcodeViewer {
 			// 
 			this->panelUnderMenu->Controls->Add(this->dataGridView1);
 			this->panelUnderMenu->Location = System::Drawing::Point(0, 47);
-			this->panelUnderMenu->Name = L"panelUnderMenu";
-			this->panelUnderMenu->Size = System::Drawing::Size(206, 528);
 			this->panelUnderMenu->MinimumSize = System::Drawing::Size(0, 150);
+			this->panelUnderMenu->Name = L"panelUnderMenu";
+			this->panelUnderMenu->Size = System::Drawing::Size(356, 528);
 			this->panelUnderMenu->TabIndex = 2;
 			// 
 			// dataGridView1
 			// 
+			this->dataGridView1->AllowUserToResizeColumns = false;
+			this->dataGridView1->AllowUserToResizeRows = false;
 			this->dataGridView1->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::ColumnHeader;
 			this->dataGridView1->BackgroundColor = System::Drawing::SystemColors::GradientInactiveCaption;
 			this->dataGridView1->CellBorderStyle = System::Windows::Forms::DataGridViewCellBorderStyle::SunkenVertical;
+			this->dataGridView1->ClipboardCopyMode = System::Windows::Forms::DataGridViewClipboardCopyMode::EnableWithoutHeaderText;
 			this->dataGridView1->ColumnHeadersBorderStyle = System::Windows::Forms::DataGridViewHeaderBorderStyle::Single;
-			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::DisableResizing;
 			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(1) { this->Column1 });
 			this->dataGridView1->Cursor = System::Windows::Forms::Cursors::IBeam;
 			this->dataGridView1->Dock = System::Windows::Forms::DockStyle::Fill;
@@ -196,6 +203,8 @@ namespace GcodeViewer {
 			dataGridViewCellStyle1->SelectionForeColor = System::Drawing::SystemColors::Desktop;
 			dataGridViewCellStyle1->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
 			this->dataGridView1->RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			this->dataGridView1->RowHeadersWidth = 75;
+			this->dataGridView1->RowHeadersWidthSizeMode = System::Windows::Forms::DataGridViewRowHeadersWidthSizeMode::DisableResizing;
 			dataGridViewCellStyle2->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
 			dataGridViewCellStyle2->Font = (gcnew System::Drawing::Font(L"Arial", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
@@ -206,21 +215,23 @@ namespace GcodeViewer {
 			this->dataGridView1->RowTemplate->Height = 17;
 			this->dataGridView1->RowTemplate->Resizable = System::Windows::Forms::DataGridViewTriState::False;
 			this->dataGridView1->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
-			this->dataGridView1->Size = System::Drawing::Size(206, 500);
+			this->dataGridView1->ShowEditingIcon = false;
+			this->dataGridView1->ShowRowErrors = false;
+			this->dataGridView1->Size = System::Drawing::Size(356, 528);
 			this->dataGridView1->TabIndex = 1;
 			// 
 			// Column1
 			// 
 			this->Column1->DividerWidth = 2;
-			this->Column1->FillWeight = 160;
+			this->Column1->FillWeight = 256;
 			this->Column1->Frozen = true;
 			this->Column1->HeaderText = L"Команды";
-			this->Column1->MinimumWidth = 160;
+			this->Column1->MinimumWidth = 256;
 			this->Column1->Name = L"Column1";
 			this->Column1->ReadOnly = true;
 			this->Column1->Resizable = System::Windows::Forms::DataGridViewTriState::False;
 			this->Column1->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::NotSortable;
-			this->Column1->Width = 160;
+			this->Column1->Width = 256;
 			// 
 			// textUnderMenu
 			// 
@@ -230,10 +241,10 @@ namespace GcodeViewer {
 			this->textUnderMenu->Name = L"textUnderMenu";
 			this->textUnderMenu->ReadOnly = true;
 			this->textUnderMenu->ShortcutsEnabled = false;
-			this->textUnderMenu->Size = System::Drawing::Size(203, 13);
+			this->textUnderMenu->Size = System::Drawing::Size(343, 13);
 			this->textUnderMenu->TabIndex = 0;
 			this->textUnderMenu->TabStop = false;
-			this->textUnderMenu->Text = L"Test Text Maxlength char [37]";
+			this->textUnderMenu->Text = L"Test Text Maxlength char [undef]";
 			// 
 			// saveFileDialog1
 			// 
@@ -242,7 +253,7 @@ namespace GcodeViewer {
 			// statusStrip1
 			// 
 			this->statusStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->toolStripStatusLabel1 });
-			this->statusStrip1->Location = System::Drawing::Point(0, 530);
+			this->statusStrip1->Location = System::Drawing::Point(0, 578);
 			this->statusStrip1->Name = L"statusStrip1";
 			this->statusStrip1->Size = System::Drawing::Size(1008, 22);
 			this->statusStrip1->TabIndex = 3;
@@ -254,20 +265,21 @@ namespace GcodeViewer {
 			this->toolStripStatusLabel1->Margin = System::Windows::Forms::Padding(3, 2, 0, 2);
 			this->toolStripStatusLabel1->Name = L"toolStripStatusLabel1";
 			this->toolStripStatusLabel1->Overflow = System::Windows::Forms::ToolStripItemOverflow::Never;
-			this->toolStripStatusLabel1->Size = System::Drawing::Size(350, 18);
+			this->toolStripStatusLabel1->Size = System::Drawing::Size(440, 18);
 			this->toolStripStatusLabel1->Text = L"statusLabel";
+			this->toolStripStatusLabel1->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1008, 600);
-			this->MinimumSize = System::Drawing::Size(480, 300);
 			this->Controls->Add(this->statusStrip1);
 			this->Controls->Add(this->panelUnderMenu);
 			this->Controls->Add(this->textUnderMenu);
 			this->Controls->Add(this->toolStrip1);
 			this->Controls->Add(this->glControl1);
+			this->MinimumSize = System::Drawing::Size(585, 300);
 			this->Name = L"MainForm";
 			this->Text = L"GcodeViewer";
 			this->Resize += gcnew System::EventHandler(this, &MainForm::MainForm_Resize);
@@ -293,18 +305,34 @@ namespace GcodeViewer {
 
 	//обработка нажатия кнопки меню "открыть"
 	private: System::Void открытьToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
-		this->openFileDialog1->ShowDialog();
-		opndfileName = this->openFileDialog1->FileName;
-		this->Text = "GcodeViewer -  файл " + opndfileName + " открыт";
+		if (System::Windows::Forms::DialogResult::OK == this->openFileDialog1->ShowDialog()) {
+			opndfileName = this->openFileDialog1->FileName;
+			this->toolStripStatusLabel1->Text = "файл " + opndfileName + " открыт";
+			this->dataGridView1->Rows->Clear();
+			tText = MainForm::getText(opndfileName);
+			this->dataGridView1->RowCount = tText->Length;
+			int rowNumber = 1;
+			System::Collections::IEnumerator^ myEnum = safe_cast<System::Collections::IEnumerable^>(dataGridView1->Rows)->GetEnumerator();
+			myEnum->Reset();
+			while (myEnum->MoveNext())
+			{
+				DataGridViewRow^ row = safe_cast<DataGridViewRow^>(myEnum->Current);
+				row->HeaderCell->Value = String::Format(L"{0}", rowNumber);
+				row->SetValues(tText[rowNumber-1]);
+				if (rowNumber >= tText->Length)
+					break;
+				rowNumber = rowNumber + 1;
+			}
+		}
+		
 	}
 	
 	//обработка изменения размеров окна
 	private: System::Void MainForm_Resize(System::Object^  sender, System::EventArgs^  e) {
-		this->glControl1->Size = System::Drawing::Size(this->Width - 232, this->Height - 92);
-		this->panelUnderMenu->Size = System::Drawing::Size(206, this->Height - 111);
+		this->glControl1->Size = System::Drawing::Size(this->Width - 380, this->Height - 92);
+		this->panelUnderMenu->Size = System::Drawing::Size(356, this->Height - 111);
 		this->toolStrip1->Size = System::Drawing::Size(this->Width - 4, 25);
-		this->statusStrip1->Size = System::Drawing::Size(this->Width - 4, 22);
-		this->Text = "" + this->panelUnderMenu->Size.Height;
+	
 	}
 
 };
